6da54ffb0cb1e0b693c6463b3beafed5e01a0a22
nekokaito <siddiqsazzad001@gmail.com>
2024-01-19 21:46:52 +0600

    added slider with daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        }
+      },
     },
     plugins: [],
   }
